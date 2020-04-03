740636765f7da3d023c760f5c67940fd07388535
Thaweesit Phukhiaopad <sci.tws@gmail.com>
2020-04-03 22:44:32 +0700

    delete item

--- src/Pages/SixthComponent.js
+++ src/Pages/SixthComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useForm } from "react-hook-form";
 import { RHFInput } from "react-hook-form-input";
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Announce from "../components/announce";
 // import Todo from '../components/Todo';
 import "../announce.css";
@@ -18,33 +19,49 @@ import {
   Button
 } from "react-bootstrap";
 
-export default () => {
-  // state = {
-  //   todos: [
-  //     { task: "Wash Dishes" },
-  //     { task: "Walk Dog" },
-  //     { task: "Brush Teeth" }
-  //   ],
-  //   inputText: "",
-  //   boxShowing: false
-  // }
+const Item = (props) => (
+  <div onClick={props.click}>{props.data.task}</div>
+)
 
-  // handleDelete = (i) => {
-  //   let newTasks = this.state.todos.slice()
-  //   newTasks.splice(i, 1)
-  //   this.setState({ todos: newTasks })
-  // }
+export default class SixthComponent extends React.Component {
+  state = {
+    todos: [
+      { task: "Wash Dishes" },
+      { task: "Walk Dog" },
+      { task: "Brush Teeth" }
+    ],
+    inputText: "",
+    boxShowing: false
+  }
 
-  return (
-    <div className="component sixth-component">
-      <Container className="center-block" style={{ alignSelf: "center" }}>
-        <h2>จากปรากดการณ์</h2>
-        {/* <div>
-          <TodoList todos={this.todos} handleDelete={handleDelete} />
-          <Box isShowing={this.boxShowing} />
-        </div> */}
-        {/* <Announce /> */}
-      </Container>
-    </div>
-  );
+  handleDelete = (i) => {
+    let newTasks = this.state.todos.slice()
+    newTasks.splice(i, 1)
+    this.setState({ todos: newTasks })
+  }
+
+  render() {
+    return (
+      <div className="component sixth-component">
+        <Container className="center-block" style={{ alignSelf: "center" }}>
+          <h2>จากปรากดการณ์</h2>
+          <div>
+            <ReactCSSTransitionGroup
+              transitionName="fade"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}>
+              { this.state.todos.map((data, index) => (
+                <Item key={index} data={data} click={() => this.handleDelete(index)} />
+              )) }
+            </ReactCSSTransitionGroup>
+          </div>
+          {/* <div>
+            <TodoList todos={this.todos} handleDelete={handleDelete} />
+            <Box isShowing={this.boxShowing} />
+          </div>
+          {/* <Announce /> */}
+        </Container>
+      </div>
+    )
+  }
 };
